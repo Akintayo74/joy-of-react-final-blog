71d48073ed7e3e1fda77539b5051e33322425885
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-11-29 14:41:13 +0100

    Use dynamic segments to pull blog data

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -4,7 +4,13 @@ import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
 
-function BlogPost() {
+import { loadBlogPost } from '@/helpers/file-helpers';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+
+async function BlogPost({ params }) {
+  const { postSlug } = await params;
+  const loadBlog = await loadBlogPost(postSlug);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
@@ -12,12 +18,7 @@ function BlogPost() {
         publishedOn={new Date()}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={loadBlog.content} />
       </div>
     </article>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -14,14 +14,6 @@ async function Home() {
         Latest Content:
       </h1>
 
-      {/* TODO: Iterate over the data read from the file system! */}
-      {/* <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      /> */}
-
       {blogList.map((blog) => {
         return <BlogSummaryCard
           key={blog.slug}
